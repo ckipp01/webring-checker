5b712a6be42711e60cebe45a3137fdf327c8addc
ckipp01 <ckipp@pm.me>
2019-05-03 08:21:04 +0200

    add in some err catches and cleanup

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const request = require('request')
 
-module.exports = async (req, res) => {
-  const sites = await gatherSites()
-  const report = await checkSites(sites)
-  res.end(JSON.stringify(report))
+module.exports = async (_, res) => {
+  try {
+    const sites = await gatherSites()
+    const report = await checkSites(sites)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(JSON.stringify(report))
+  } catch (err) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' })
+    res.end(err)
+  }
 }
 
 const checkSites = async list => {
   return new Promise((resolve, reject) => {
     const results = list.map(checkUrl)
     Promise.all(results)
-      .then(data => {
-        resolve(data)
-      })
+      .then(data => { resolve(data) })
+      .catch(err => { reject(err) })
   })
 }
 
 const checkUrl = url => {
   return new Promise((resolve, reject) => {
-    request(url, (error, response) => {
-      const sc = response.statusCode
-      if (!error) resolve({ url, sc })
+    request(url, (err, response) => {
+      const statusCode = response.statusCode
+      if (!err) resolve({ url, statusCode })
+      else reject(err)
     })
   })
 }
 
 const gatherSites = () => {
   return new Promise((resolve, reject) => {
-    request('https://webring.xxiivv.com/scripts/sites.js', (error, response, body) => {
-      if (!error) {
+    request('https://webring.xxiivv.com/scripts/sites.js', (err, _, body) => {
+      if (!err) {
         const begin = body.indexOf('[') + 1
         const end = body.indexOf(']')
 
@@ -38,7 +44,7 @@ const gatherSites = () => {
           .map(url => url.trim())
           .map(url => url.replace(/"/g, ''))
         resolve(sites)
-      }
+      } else reject(err)
     })
   })
 }
