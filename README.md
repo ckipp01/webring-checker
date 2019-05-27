01d410e8252f4299658ce195e411847e69da15af
ckipp01 <ckipp@pm.me>
2019-05-27 07:13:46 +0200

    change styling to match webring a bit better

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Small api and service to check the status of the sites in [the webring](https://
   - `GET / will return a description page`
   - `GET /check?format=html` will return an html report of all sites, their status code, and last modified date if available
   - `GET /check?format=json` will return the same info as the html format, but in json
-  - `GET /sites` will return a json array of sites that are part of the webring
+  - `GET /sites` will return a json array of siteObjects that are part of the webring
--- api/checker.js
+++ api/checker.js
@@ -51,26 +51,40 @@ const checkUrl = siteObject => {
           ? response.headers['last-modified']
           : 'No last modified info'
         const statusCode = response.statusCode
-        if (!err) resolve({ url: siteObject.url, statusCode, lastModified, title: siteObject.title })
-        else reject(err)
+        if (!err) {
+          resolve({
+            url: siteObject.url,
+            statusCode,
+            lastModified,
+            title: siteObject.title,
+            type: siteObject.type
+          })
+        } else reject(err)
       } else {
-        resolve({ url: siteObject.url, statusCode: '???', lastModified: 'Unable to retreive a status code.' })
+        // TODO remove duplication below and above
+        resolve({
+          url: siteObject.url,
+          statusCode: '???',
+          lastModified: 'Unable to retreive a status code.',
+          title: siteObject.title,
+          type: siteObject.type
+        })
       }
     })
   })
 }
 
 const captureOffenders = (sites, site, i) => {
-  const style = site.statusCode > 200 || site.statusCode === '???'
-    ? ` style="color:#F03"`
+  const errorClass = site.statusCode > 200 || site.statusCode === '???'
+    ? 'error'
     : ''
+  const siteType = site.type !== undefined ? site.type : ''
+
   return `${sites}
-    <tr${style}>
+    <tr${errorClass}>
       <td>${i})</td>
-      <td>
-        <a target="_blank" href="${site.url}">
-        ${site.title !== undefined ? site.title : site.url.split('//')[1]}
-        </a>
+      <td class="${siteType}">
+        <a target="_blank" href="${site.url}">${site.title !== undefined ? site.title : site.url.split('//')[1]}</a>
       </td>
       <td>${site.statusCode}</td>
       <td>${site.lastModified}</td>
@@ -79,36 +93,16 @@ const captureOffenders = (sites, site, i) => {
 
 const style = `
   * { margin:0;padding:0;border:0;outline:0;text-decoration:none;font-weight:inherit;font-style:inherit;color:inherit;font-size:100%;font-family:inherit;vertical-align:baseline;list-style:none;border-collapse:collapse;border-spacing:0; -webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}
-
-  body {
-    background:#111;
-    padding:0px; margin:0px;
-  }
-  div {
-    background: #f4f4f4;
-    margin: 15px;
-    color: #333;
-    padding: 15px;
-    font-family:'Monospaced','Courier New',courier;
-    font-size:12px;
-    border-radius: 2px;
-  }
-  th {
-    font-size: 13px;
-    border-bottom: 2px solid;
-  }
-  a {
-    text-decoration: underline black;
-  }
-  tr {
-    line-height: 20px;
-  }
-  td {
-    padding: 0 0.5em;
-  }
-  h5 {
-    margin: 1em
-  }
+  body { background:#111; padding:0px; margin:0px; }
+  div { background: #f4f4f4;margin: 15px;color: #333;padding: 15px;font-family:'Monospaced','Courier New',courier;font-size:12px;border-radius: 2px; }
+  th { font-size: 13px;border-bottom: 2px solid; }
+  a { padding: 0 0 0 1em; }
+  a:hover { text-decoration: underline black; }
+  td.wiki::after { content:"<wiki>"; color: #A8A8A8; }
+  td.blog::after { content:"<blog>"; color: #A8A8A8; }
+  tr { line-height: 20px; }
+  tr.error { color:#F03; }
+  h5 { margin: 1em }
 `
 
 const htmlify = list => {
