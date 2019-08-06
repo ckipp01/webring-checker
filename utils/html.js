1b4ad6721e69f8d5c284b7401d2971dd4b5badb4
DYM <re@dym.sh>
2019-08-06 18:30:55 +0200

    fix not showing custom site types

--- utils/html.js
+++ utils/html.js
@@ -7,14 +7,11 @@ const captureOffenders = (sites, site, i) => {
     ? 'error'
     : ''
 
-  const siteType = site.type !== undefined ? site.type : ''
-
   return `${sites}
     <tr class="${errorClass}">
       <td>${i})</td>
-      <td class="${siteType}">
-        <a target="_blank" href="${site.url}">${site.title !== undefined ? site.title : site.url.split('//')[1]}</a>
-      </td>
+      <td><a target="_blank" href="${site.url}">${site.title || site.url.split('//')[1]}</a></td>
+      <td>${site.type || ''}</td>
       <td>${site.statusCode}</td>
       <td>${site.lastModified}</td>
     </tr>`
@@ -34,6 +31,7 @@ export const htmlifyReport = list => {
                               <tr>
                                 <th></th>
                                 <th>Url</th>
+                                <th>Type</th>
                                 <th>Status</th>
                                 <th>Last Modified</th>
                               </thead>
